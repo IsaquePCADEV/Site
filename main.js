e53a5fdfc9150f2d7167da0aa94337fd6faaa05f
IsaquePCA <isaquemaster07@gmail.com>
2022-09-29 15:53:19 -0300

    .

--- main.js
+++ main.js
@@ -7,9 +7,9 @@ menuBar.addEventListener('click', function() {
     if (iconMenu.getAttribute("src") == 'imagens/menu.png') {
         iconMenu.setAttribute("src", "imagens/close.png");
     }else {
-        iconMenu.setAttribute("src", "imagens/menu.png")
+        iconMenu.setAttribute("src", "imagens/menu.png");
     }
 
     menu.classList.toggle('active');
-    
+
 });
